69853fa14926537525f6b28684a53b02d22194df
YkTru <astramistil@hotmail.com>
2024-12-17 10:55:52 -0500

    removed superflous stuff

--- MRE.Core/Program.fs
+++ MRE.Core/Program.fs
@@ -7,193 +7,89 @@ open Serilog.Extensions.Logging
 open Elmish
 
 module App =
+
     type TreeItem =
         { Id: Guid
           Data: string
           Children: TreeItem list }
 
-    [<AutoOpen>]
-    module TreeItem =
-
-        let create data =
+    let buildMockTree () =
+        let node111 =
             { Id = Guid.NewGuid()
-              Data = data
+              Data = "1.1.1"
               Children = [] }
 
-        let moveItemUp (id: Guid) (children: TreeItem list) : TreeItem list =
-            let rec aux acc =
-                function
-                | [] -> List.rev acc
-                | x :: xs when x.Id = id ->
-                    match acc with
-                    | [] -> x :: xs // Can't move up, already at top
-                    | y :: rest -> List.rev rest @ (x :: y :: xs)
-                | x :: xs -> aux (x :: acc) xs
+        let node112 =
+            { Id = Guid.NewGuid()
+              Data = "1.1.2"
+              Children = [] }
 
-            aux [] children |> List.toArray |> Array.toList
+        let node113 =
+            { Id = Guid.NewGuid()
+              Data = "1.1.3"
+              Children = [] }
 
-        let moveItemDown (id: Guid) (children: TreeItem list) : TreeItem list =
-            let rec aux acc =
-                function
-                | [] -> List.rev acc
-                | x :: (y :: ys) when x.Id = id -> List.rev acc @ (y :: x :: ys)
-                | x :: xs -> aux (x :: acc) xs
+        let node11 =
+            { Id = Guid.NewGuid()
+              Data = "1.1"
+              Children = [ node111; node112; node113 ] }
 
-            aux [] children |> List.toArray |> Array.toList
+        let node12 =
+            { Id = Guid.NewGuid()
+              Data = "1.2"
+              Children = [] }
 
-   
-        let rec findItemById (id: Guid) (t: TreeItem) : TreeItem option =
-            if t.Id = id then Some t else t.Children |> List.tryPick (findItemById id)
+        let node1 =
+            { Id = Guid.NewGuid()
+              Data = "1"
+              Children = [ node11; node12 ] }
 
-        let rec updateTreeItemById id f tree =
-            if tree.Id = id then
-                printfn "Updating tree item with ID: %O" id
-                f tree
-            else
-                { tree with
-                    Children =
-                        tree.Children
-                        |> List.map (updateTreeItemById id f)
-                        |> List.toArray // Ensure new instance
-                        |> Array.toList }
+        let node2111 =
+            { Id = Guid.NewGuid()
+              Data = "2.1.1.1"
+              Children = [] }
 
+        let node21 =
+            { Id = Guid.NewGuid()
+              Data = "2.1"
+              Children = [ node2111 ] }
 
-        let rec updateParentOfItemById
-            (id: Guid)
-            (f: TreeItem list -> TreeItem list)
-            (tree: TreeItem)
-            : TreeItem =
-            if tree.Children |> List.exists (fun child -> child.Id = id) then
-                { tree with Children = f tree.Children }
-            else
-                { tree with Children = tree.Children |> List.map (updateParentOfItemById id f) }
+        let node2 =
+            { Id = Guid.NewGuid()
+              Data = "2"
+              Children = [ node21 ] }
 
+        let node3 =
+            { Id = Guid.NewGuid()
+              Data = "3"
+              Children = [] }
 
-        module Mock =
+        { Id = Guid.NewGuid()
+          Data = "Root"
+          Children = [ node1; node2; node3 ] }
 
-            let buildMockTree () =
-                let node111 = { Id = Guid.NewGuid(); Data = "1.1.1"; Children = [] }
-                let node112 = { Id = Guid.NewGuid(); Data = "1.1.2"; Children = [] }
-                let node113 = { Id = Guid.NewGuid(); Data = "1.1.3"; Children = [] }
-                let node11 = { Id = Guid.NewGuid(); Data = "1.1"; Children = [ node111; node112; node113 ] }
-                
-                let node12 = { Id = Guid.NewGuid(); Data = "1.2"; Children = [] }
-        
-                let node1 = { Id = Guid.NewGuid(); Data = "1"; Children = [ node11; node12 ] }
-                
-                let node2111 = { Id = Guid.NewGuid(); Data = "2.1.1.1"; Children = [] }
-                let node21 = { Id = Guid.NewGuid(); Data = "2.1"; Children = [ node2111 ] }
-                let node2 = { Id = Guid.NewGuid(); Data = "2"; Children = [ node21 ] }
-                
-                let node3 = { Id = Guid.NewGuid(); Data = "3"; Children = [] }
-                
-                { Id = Guid.NewGuid()
-                  Data = "Root"
-                  Children = [ node1; node2; node3 ] }
-        
-
-                 
 
     type Model =
         { Tree: TreeItem
-          SelectedItem: Guid option
-          FocusedItem: Guid option
-          SelectedItemLogMessage: string }
+          SelectedItem: Guid option }
 
-    type Msg =
-        | SelectItem of Guid option
-        | UpdateSelectedItemData of string
-        | MoveUp
-        | MoveDown
+    type Msg = SelectItem of Guid option
 
     let init () =
-        { Tree = Mock.buildMockTree ()
-          SelectedItem = None
-          FocusedItem = None
-          SelectedItemLogMessage = "No Item Selected" },
+        { Tree = buildMockTree ()
+          SelectedItem = None },
         Cmd.none
-
-
-    let selectItem idOpt model =
-        let logMsg =
-            match idOpt with
-            | Some id -> sprintf "Selected Item: %O" id
-            | None -> "No Item Selected"
-
-        printfn "Updated SelectedItem: %O" idOpt
-
-        { model with
-            SelectedItem = idOpt
-            FocusedItem = idOpt // WPF TreeView: Also set the focused item
-            SelectedItemLogMessage = logMsg }
 
     let update msg model : Model * Cmd<Msg> =
         match msg with
-        | SelectItem idOpt ->
-            selectItem idOpt model, Cmd.none
-
-        | UpdateSelectedItemData newData ->
-            match model.SelectedItem with
-            | None -> model, Cmd.none
-            | Some id ->
-                { model with
-                    Tree =
-                        TreeItem.updateTreeItemById
-                            id
-                            (fun item -> { item with Data = newData })
-                            model.Tree },
-                Cmd.none
-
-        | MoveUp ->
-            match model.SelectedItem with
-            | None -> model, Cmd.none
-            | Some id ->
-                let updatedTree =
-                    TreeItem.updateParentOfItemById
-                        id
-                        (fun children -> TreeItem.moveItemUp id children)
-                        model.Tree
-
-                { model with
-                    Tree = updatedTree
-                    FocusedItem = Some id }, // Focus item in the model
-                Cmd.ofMsg (SelectItem(Some id)) // Ensure item is re-selected
-
-        | MoveDown ->
-            match model.SelectedItem with
-            | None -> model, Cmd.none
-            | Some id ->
-                let updatedTree =
-                    TreeItem.updateParentOfItemById
-                        id
-                        (fun children -> TreeItem.moveItemDown id children)
-                        model.Tree
-
-                { model with
-                    Tree = updatedTree
-                    FocusedItem = Some id }, // Focus item in the model
-                Cmd.ofMsg (SelectItem(Some id)) // Ensure item is re-selected
-
-
+        | SelectItem idOpt ->  { model with SelectedItem = idOpt }, Cmd.none
 
 
 module Bindings =
     open App
 
-
-    let twoWayDataBinding =
-        Binding.twoWay (
-            (fun (t: TreeItem) -> t.Data),
-            (fun (newData: string) (_t: TreeItem) -> UpdateSelectedItemData newData)
-        )
-
-
     let rec treeItemBindings () : Binding<TreeItem, Msg> list =
-        [ "Data"
-          |> Binding.twoWay (
-              (fun (t: TreeItem) -> t.Data),
-              (fun (newData: string) _ -> UpdateSelectedItemData newData)
-          )
+        [ "Data" |> Binding.oneWay ((fun (t: TreeItem) -> t.Data))
 
           "Children"
           |> Binding.subModelSeq (
@@ -206,17 +102,7 @@ module Bindings =
 
           ]
 
-    let getSelectedItemData (m: Model) =
-        match m.SelectedItem with
-        | Some sid ->
-            match App.TreeItem.findItemById sid m.Tree with
-            | Some item -> item.Data
-            | None -> ""
-        | None -> ""
 
-    let setSelectedItemData (newData: string) (m: Model) = UpdateSelectedItemData newData
-
-    //• Root bindings
     let rootBindings () : Binding<Model, Msg> list =
         [ "Tree"
           |> Binding.SubModel.required (treeItemBindings)
@@ -238,21 +124,7 @@ module Bindings =
               (fun idOpt _ -> SelectItem idOpt)
           )
 
-
-          "FocusedItem" |> Binding.oneWay (fun m -> m.FocusedItem) // Pass the focused item to the behavior
-
-
-          "SelectedItemData"
-          |> Binding.twoWay (
-              (fun m -> getSelectedItemData m),
-              (fun newData m -> setSelectedItemData newData m)
-          )
-
-          "SelectedItemLogMessage" |> Binding.oneWay (fun m -> m.SelectedItemLogMessage)
-
-          "MoveUp" |> Binding.cmdIf (fun m -> if m.SelectedItem.IsSome then Some MoveUp else None)
-          "MoveDown"
-          |> Binding.cmdIf (fun m -> if m.SelectedItem.IsSome then Some MoveDown else None) ]
+          ]
 
 
 //------------------------------------------------------------------------------------------------
